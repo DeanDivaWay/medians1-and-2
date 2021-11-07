2724186270e80fdf4efd3937d74e19b08dca4f4c
DeanDivaWay <92186059+DeanDivaWay@users.noreply.github.com>
2021-11-07 14:26:48 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+nums1= list(map(int, input().split()))
+nums2 = list(map(int, input().split()))
+
+def medians(nums1,nums2):
+    My_exellentn_ums_ = nums1 + nums2
+    My_exellentn_ums_.sort()
+    length = len (My_exellentn_ums_)
+    length = sizeee
+if sizeee % 2 != 0:
+    My1____mediana = My_exellentn_ums_[int((sizeee+1)/2 - 1)]
+    print(float(My1____mediana))
+    print(My_exellentn_ums_, My1____mediana)
+else:
+    My2____mediana = (My_exellentn_ums_[int(sizeee/ 2 - 1)] + My_exellentn_ums_[int(sizeee/ 2)]) / 2
+    print(float(My2____mediana))
+    print(My_exellentn_ums_, My2____mediana)
+
+
+medians(nums1,nums2)
